42940b22e9c09c09c18ad9604f478545556bbc88
Gehad Makhlouf <gehadmakhlouf@gmail.com>
2018-06-07 22:23:10 +0300

    think python Ex-2.3(1)

---
+++ Ex-2.3(1).py
@@ -0,0 +1,10 @@
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> pi = 3.1415926535897932
+>>> 4.0/3
+1.3333333333333333
+>>> 5**3
+125
+>>> 4.0/3*pi*5**3
+523.5987755982989
+>>> 
